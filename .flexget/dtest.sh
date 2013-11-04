d156092b818c03c422811065098cd5d4e269580c
ZujkisNx <pauliusruzgas@gmail.com>
2013-11-04 19:52:19 +0200

    added flexget config and cron file

---
+++ .flexget/dtest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+/usr/local/bin/flexget
+
+#export DISPLAY=:0
+#/usr/bin/transmission -m
+
+export DISPLAY=:0
+cd /usr/bin/
+transmission -m
+
+#Play a sound
+#mplayer -msglevel all=-1 ~/Public/soudbits/mariobros/bazooka.wav
